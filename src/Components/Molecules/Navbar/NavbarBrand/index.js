db350a2842b0d36af48f15d5b9f2918aeded83d9
DeriFirgiawan <derifirgiawan025@gmail.com>
2021-03-12 18:03:00 +0700

    Fix error in navbar-brand

--- src/Components/Molecules/Navbar/NavbarBrand/index.js
+++ src/Components/Molecules/Navbar/NavbarBrand/index.js
@@ -1,11 +1,11 @@
 import Linked from '../../../Atom/Linked'
 
-import LogoDesktop from '../../../../Assets/Icons/icon.svg'
+import Logo from '../../../../Assets/Icons/icon.svg'
 
 export const NavbarBrand = () => {
   return (
-    <Linked to="/">
-      <img src={LogoDesktop} alt="Facebook"/>
+    <Linked url="/">
+      <img src={Logo} alt="Facebook"/>
     </Linked>
   )
 }
