20142f3634f1c24e27d1083c161bda307dbdce78
DeriFirgiawan <derifirgiawan025@gmail.com>
2021-03-12 17:53:02 +0700

    Refactor Router user access

--- src/Routers/index.js
+++ src/Routers/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import {connect} from 'react-redux'
 
@@ -8,28 +8,47 @@ import Dashboard from '../Container/Pages/Dashboard'
 import SideHome from '../Components/Molecules/SideHome'
 
 class RouterHome extends React.Component {
-  render() {
+  state = {
+    fullName: "",
+    shortName: "",
+  }
+
+  componentDidMount() {
     const getDataUser = JSON.parse(localStorage.getItem('userData'))
-    const fullName = getDataUser.displayName
-    const shortName = fullName.split(' ')
+    if (this.props.userIsLogin || getDataUser) {
+      const fullName = getDataUser.displayName
+      const shortName = fullName.split(' ')
+      this.setState({
+        fullName: fullName,
+        shortName: shortName[0]
+      })
+    } else {
+      this.props.history.push("/login")
+    }
+  }
+  render() {
+    const {fullName, shortName} = this.state
     return (
-      <Router>
-        <Templates shortName={shortName[0]} fullName={fullName}
-        sidebar={
-          <Route path="/home" exact>
-            <SideHome fullName={fullName} />
-          </Route>
-        } 
-        content={
-          <Route path="/home" exact component={Dashboard} />
-        } />
-      </Router>
+      <Fragment>
+        <Router>
+          <Templates shortName={shortName} fullName={fullName}
+          sidebar={
+            <Route path="/" exact>
+              <SideHome fullName={fullName} />
+            </Route>
+          } 
+          content={
+            <Route path="/" exact component={Dashboard} />
+          } />
+        </Router>
+      </Fragment>
     )
   }
 }
 
 const reduxState = (state) => ({
   userName: state.user,
+  userIsLogin: state.isLogin
 })
 
 export default connect(reduxState, null)(RouterHome)
