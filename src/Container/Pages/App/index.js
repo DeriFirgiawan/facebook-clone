8c81dbae90f68530175320963b00c8cd529c7561
DeriFirgiawan <derifirgiawan025@gmail.com>
2021-03-12 18:05:52 +0700

    Import new Component Auth

--- src/Container/Pages/App/index.js
+++ src/Container/Pages/App/index.js
@@ -1,17 +1,17 @@
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import {store} from '../../../Config/Redux'
-import Login from '../Login'
+import AUTH_LOGIN_REGISTER from '../../Feature/Auth'
 import RouterHome from '../../../Routers'
 
-import './App.scss'
+import './_App.module.scss'
 
 export default function App() {
   return (
     <Provider store={store}>
       <Router>
-        <Route path="/" exact component={Login} />
-        <Route path="/home" exact component={RouterHome} />
+        <Route path="/login" exact component={AUTH_LOGIN_REGISTER} />
+        <Route path="/" exact component={RouterHome} />
       </Router>
     </Provider>
   )
